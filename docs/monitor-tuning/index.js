e7aa88e6009c0c8b8d6e93c3320ab399b761558a
skyfalling <liyifei@kuaishou.com>
2022-02-09 14:36:05 +0800

    init

---
+++ docs/monitor-tuning/index.js
@@ -0,0 +1,80 @@
+module.exports = () => {
+  return [
+    '',
+    '00.md',
+    {
+      title: '基于 JDK 命令行工具的监控',
+      path:'/monitor-tuning/01/',
+      collapsable: true,
+      children: [
+        '01/01.md',
+        '01/02.md',
+        '01/03.md',
+        '01/04.md',
+        '01/05.md'
+      ]
+    },
+    {
+      title: '基于 JVisualVM 的可视化监控',
+      path:'/monitor-tuning/02/',
+      collapsable: true,
+      children: [
+        '02/01.md',
+        '02/02.md'
+      ]
+    },
+    {
+      title: '基于 Btrace 的监控调试',
+      path:'/monitor-tuning/03/',
+      collapsable: true,
+      children: [
+        '03/01.md',
+        '03/02.md'
+      ]
+    },
+    {
+      title: 'Tomcat 性能监控与调优',
+      path:'/monitor-tuning/04/',
+      collapsable: true,
+      children: [
+        '04/01.md',
+        '04/02.md',
+        '04/03.md',
+        '01/04.md'
+      ]
+    },
+    {
+      title: 'Nginx 性能监控与调优',
+      path:'/monitor-tuning/05/',
+      collapsable: true,
+      children: [
+        '05/01.md',
+        '05/02.md',
+        '05/03.md',
+        '05/04.md'
+      ]
+    },
+    {
+      title: 'JVM 层 GC 调优',
+      path:'/monitor-tuning/06/',
+      collapsable: true,
+      children: [
+        '06/01.md',
+        '06/02.md',
+        '06/03.md',
+        '06/04.md',
+        '06/05.md'
+      ]
+    },
+    {
+      title: 'Java 代码层优化',
+      path:'/monitor-tuning/07/',
+      collapsable: true,
+      children: [
+        '07/01.md',
+        '07/02.md',
+        '07/03.md'
+      ]
+    }
+  ]
+}
